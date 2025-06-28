fd04fbce50c0d39f2a0a8d9c31568ca1f66cfb59
shummiee <saCena@mcm.edu.ph>
2025-06-29 00:43:41 +0800

    login

--- FSFC_OSFinalProject/Login.Designer.cs
+++ FSFC_OSFinalProject/Login.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            pictureBox1 = new PictureBox();
+            txtUsername = new TextBox();
+            gi = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-1, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(681, 414);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(291, 177);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(192, 23);
+            txtUsername.TabIndex = 1;
+            // 
+            // gi
+            // 
+            gi.Location = new Point(291, 226);
+            gi.Name = "gi";
+            gi.Size = new Size(192, 23);
+            gi.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(0, 0, 64);
+            button1.Location = new Point(301, 268);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 29);
+            button1.TabIndex = 3;
+            button1.Text = "LOGIN";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(680, 410);
+            Controls.Add(button1);
+            Controls.Add(gi);
+            Controls.Add(txtUsername);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Login";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox txtUsername;
+        private TextBox gi;
+        private Button button1;
     }
 }
